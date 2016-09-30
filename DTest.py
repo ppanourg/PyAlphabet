408e26b09ac5258453a818f542b6c1d11db85867
Peter <peter.panourgias@gmail.com>
2016-09-30 00:49:26 -0700

    Updated DTest

--- DTest.py
+++ DTest.py
@@ -5,27 +5,27 @@ dict = Dictionary()
 
 """Frequency orders of the below added elements:
 
-   Starts with: D                       A              P         
-                Don't vote for trump    Ammmurrica     Polynomial
-                Dank                    Aardvark       Pandora's box
+   Starts with: D                       A              P
+                Don't do it             Ammmurrica     Polynomial
+                Darn                    Aardvark       Pandora's box
                 Derp                    Abacus         Polly wanna cracker?
                                                                              """
 
 print("\n\nPandora's box inserted?: " + str(dict.insert("Pandora's box", 1232)))
 print("Derp inserted?: " + str(dict.insert("Derp", -5)))
 print("Aardvark inserted?: " + str(dict.insert("Aardvark", 55)))
-print("Dank inserted?: " + str(dict.insert("Dank", 123)))
+print("Darn inserted?: " + str(dict.insert("Darn", 123)))
 print("Polynomial inserted?: " + str(dict.insert("Polynomial", 12333)))
 print("Abacus inserted?: " + str(dict.insert("Abacus", 4)))
-print("'Don't vote for trump' inserted?: " + str(dict.insert("Don't vote for Trump.", 9**5)))
+print("'Don't do it' inserted?: " + str(dict.insert("Don't do it.", 9**5)))
 print("'Polly wanna cracker?' inserted?: " + str(dict.insert("Polly wanna cracker?", 1231)))
 print("Ammmurrica inserted?: " + str(dict.insert("Ammmurrica", 555)))
 
 print("\n\nDerp reinserted?: " + str(dict.insert("Derp", -5)))
 print("Aardvark reinserted?: " + str(dict.insert("Aardvark", 55)))
-print("Dank reinserted?: " + str(dict.insert("Dank", 123)))
+print("Darn reinserted?: " + str(dict.insert("Darn", 123)))
 print("Abacus reinserted?: " + str(dict.insert("Abacus", 4)))
-print("'Don't vote for trump' inserted?: " + str(dict.insert("Don't vote for Trump.", 9**5)))
+print("'Don't do it' inserted?: " + str(dict.insert("Don't do it.", 9**5)))
 print("Ammmurrica reinserted?: " + str(dict.insert("Ammmurrica", 555)))
 
 
@@ -34,16 +34,16 @@ print("Polynomial found: " + str(dict.find("Polynomial")))
 print("'Polly wanna cracker?' found?: " + str(dict.find("Polly wanna cracker?")))
 print("\n\nDerp found?: " + str(dict.find("Derp")))
 print("Aardvark found: " + str(dict.find("Aardvark")))
-print("Dank found?: " + str(dict.find("Dank")))
+print("Darn found?: " + str(dict.find("Darn")))
 print("Abacus found?: " + str(dict.find("Abacus")))
-print("'Don't vote for trump' found?: " + str(dict.find("Don't vote for Trump.")))
+print("'Don't do it' found?: " + str(dict.find("Don't do it.")))
 print("Ammmurrica found?: " + str(dict.find("Ammmurrica")))
 
 print("\n\nderp found?: " + str(dict.find("derp")))
 print("Ardvark found: " + str(dict.find("Ardvark")))
 print("Dunk found?: " + str(dict.find("Dunk")))
 print("Aabacus found?: " + str(dict.find("Aabacus")))
-print("'Do vote for trump' found?: " + str(dict.find("Do vote for Trump.")))
+print("'Do do it found?: " + str(dict.find("Do do it.")))
 print("Ammmurricas found?: " + str(dict.find("Ammmurricas")))
 
 print("\n\nPredict top 100 for A: " + str(dict.predict("A", 100)))
